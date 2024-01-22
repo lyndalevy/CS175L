d3b82e09ceb8bb3f1e53662196e243b1d647db3e
Lynda <144750892+lyndalevy@users.noreply.github.com>
2024-01-22 11:42:55 -0500

    My information v2

--- myInfo.py
+++ myInfo.py
@@ -5,3 +5,4 @@
 print("My name is Lynda Levy")
 print("I live at 102 Whalepond Road, Eatontown, New Jersey, 07724")
 print("My phone number is 732-759-8482")
+print("I'm a computer science major, and a graphic design & anthropology double minor")
