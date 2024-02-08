940e021cf22f57438a9ec6c98be986c3aa51ddf9
Lynda <144750892+lyndalevy@users.noreply.github.com>
2024-02-08 12:35:57 -0500

    Add files via upload

--- resturaunt.py
+++ resturaunt.py
@@ -2,22 +2,22 @@
 #Lynda Levy
 #resturaunt
 
-vegetarian = input("Is anyone in your party vegetarian? ")
-vegan = input("Is anyone in your party vegan? ")
-gluten = input("Is anyone in your party gluetn-free? ")
-    
-print(" ")
-print("Here are your resturaunt choices.")
+yOrN = "yes"
 
-if (vegetarian == "no") and (vegan == "no") and (gluten == "no"):
-    print("Joe's Gournet Burgers")
-
-if (vegan == "no") and (gluten == "no"):
-    print("Mama's Fine Italian")
-    
-if vegan == "no":
-    print("Main Street Pizza Company")
-    
-print("Corner Cafe")
-print("The Chef's Kitchen")
+while yOrN == "yes":
+    print(" ")
+    vegetarian = input("Is anyone in your party vegetarian? ")
+    vegan = input("Is anyone in your party vegan? ")
+    gluten = input("Is anyone in your party gluetn-free? ")
+    print(" ")
+    print("Here are your resturaunt choices.")
+    if (vegetarian == "no") and (vegan == "no") and (gluten == "no"):
+        print("Joe's Gournet Burgers")
+    if (vegan == "no") and (gluten == "no"):
+        print("Mama's Fine Italian")
+    if vegan == "no":
+        print("Main Street Pizza Company")
+    print("Corner Cafe")
+    print("The Chef's Kitchen")
+    yOrN = input("To ask again, write 'yes'. If not, write 'no': ")
 
