41691b565cd99d07c976a95f39ebbf647acff937
Lynda <144750892+lyndalevy@users.noreply.github.com>
2024-02-25 20:08:41 -0500

    Add files via upload

---
+++ TemperatureConversions.py
@@ -0,0 +1,42 @@
+def main():
+    controlLoop()
+
+def convertToKelvin():
+    #Take in the fahrenheit temp as an argument
+    #Convert Fahrenheit to Kelvin
+    #return Kelvin
+    pass
+
+def convertToCentigrade():
+    #Take in the fahrenheit temp as an argument
+    #Convert Fahrenheit to Centigrade
+    #return Centigrade
+    pass
+
+def doConversion():
+    #getFahrenheit(), get back Fahrenheit
+    #convertToKelvin(), send Fahrenheit get back Kelvin
+    #ConvertToCentigrade, sends Fahrenheit gets back Centigrade
+    #prints for example: 'Fahrenheit: xx Kelvin xx Centigrade: xx'
+    pass
+
+def repeat():
+    #Inputs How many conversions would you like to do this time?
+    #for x in range how many
+        #doConversion()
+    pass
+
+def controlLoop():
+    #Inputs 'Do you want to do some conversions(Yes or No)?'
+    #if 'yes' repeat()
+    pass
+
+def getFahrenheit():
+    #Inputs 'Enter Fahrenheit temperature (must be -50 to 130):'
+    #(validation loop)'Please re-enter'
+    #return Fahrenheit
+    pass
+
+# Call the main function.
+if __name__ == '__main__':
+    main()
