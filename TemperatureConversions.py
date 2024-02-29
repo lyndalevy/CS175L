9041540147783d7ead075146d08cc11d92950bd2
Lynda <144750892+lyndalevy@users.noreply.github.com>
2024-02-29 12:52:15 -0500

    Add files via upload

--- TemperatureConversions.py
+++ TemperatureConversions.py
@@ -1,41 +1,55 @@
 def main():
     controlLoop()
 
-def convertToKelvin():
+def convertToKelvin(temp):
     #Take in the fahrenheit temp as an argument
     #Convert Fahrenheit to Kelvin
+    newTemp = ((temp - 32) * (5/9) + 273.15)
     #return Kelvin
-    pass
+    return newTemp
 
-def convertToCentigrade():
+def convertToCentigrade(temp):
     #Take in the fahrenheit temp as an argument
     #Convert Fahrenheit to Centigrade
+    newTemp = (temp - 32) * (5/9)
     #return Centigrade
-    pass
+    return newTemp
 
 def doConversion():
     #getFahrenheit(), get back Fahrenheit
+    fTemp = getFahrenheit()
     #convertToKelvin(), send Fahrenheit get back Kelvin
+    kTemp = convertToKelvin(fTemp)
     #ConvertToCentigrade, sends Fahrenheit gets back Centigrade
+    cTemp = convertToCentigrade(fTemp)
     #prints for example: 'Fahrenheit: xx Kelvin xx Centigrade: xx'
-    pass
+    print(f"Farenheit: {fTemp} Kelvin: {kTemp} Centigrade: {cTemp}")
 
 def repeat():
     #Inputs How many conversions would you like to do this time?
+    repeatAmount = int(input("How many conversions would you like to do this time? "))
     #for x in range how many
+    for x in range(repeatAmount):
         #doConversion()
-    pass
+        doConversion()
 
 def controlLoop():
     #Inputs 'Do you want to do some conversions(Yes or No)?'
+    inputYorN = input("Do you want to do some conversions(Yes or No)? ")
     #if 'yes' repeat()
-    pass
+    if inputYorN == "yes":
+        repeat()
 
 def getFahrenheit():
     #Inputs 'Enter Fahrenheit temperature (must be -50 to 130):'
+    fTemp = int(input("Enter Fahrenheit temperature (must be -50 to 130): "))
     #(validation loop)'Please re-enter'
+    if -50.0 <= fTemp <= 130.0:
+        return fTemp
+    else:
+        print('Invalid temp')
     #return Fahrenheit
-    pass
+    return fTemp
 
 # Call the main function.
 if __name__ == '__main__':
